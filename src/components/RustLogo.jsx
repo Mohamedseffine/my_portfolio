03f7a34e6363b01928dac4fcf81e187bed7db0b9
mosdef <seffinemohamed470@gmail.com>
2026-05-22 12:55:45 +0100

    added main 3d model and some logos

---
+++ src/components/RustLogo.jsx
@@ -0,0 +1,24 @@
+import { useGSAP } from "@gsap/react";
+import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+import gsap from "gsap";
+
+const RustLogo = (props) => {
+  const logoRef = useRef();
+  const { scene } = useGLTF("/models/rust_logo/scene.gltf");
+  useGSAP(() => {
+    gsap.to(logoRef.current.position, {
+      y: logoRef.current.position.y + 0.5,
+      duration: 1.5,
+      repeat: -1,
+      yoyo: true,
+    });
+  });
+  return (
+    <mesh position={[-10,0,0]} ref={logoRef}  rotation={[0, 3.7, 0]} scale={1}>
+      <primitive object={scene} />
+    </mesh>
+  );
+};
+
+export default RustLogo;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,22 +1,54 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import HackerRoom from "../components/HakcerRoom.jsx"
+import HackerRoom from "../components/HakcerRoom.jsx";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import ReactLogo from "../components/ReactLogo.jsx";
+import Cube from "../components/Cube.jsx";
+import RustLogo from "../components/RustLogo.jsx";
 
 const Hero = () => {
-    return (
-        <section className=" min-h-screen w-full flex flex-col relative">
-            <div className=" w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-                <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans ">Hi, I am Mohamed <span className="waving-hand">👋</span></p>
-                <p className="hero_tag text-gray_gradient">Securing systems & Finding Bugs</p>
-            </div>
-            <div className="w-full h-full absolute inset-0">
-                <Canvas className="w-full h-full">
-                    <PerspectiveCamera makeDefault position={[0,0,30]} />
-                    <HackerRoom />
-                </Canvas>
-            </div>
-        </section>
-    )
-}
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
-export default Hero
+  return (
+    <section className=" min-h-screen w-full flex flex-col relative">
+      <div className=" w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans ">
+          Hi, I am Mohamed Seffine<span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Securing Systems & Finding Flaws
+        </p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              scale={sizes.deskScale}
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+            />
+            <group>
+                <Target position={sizes.targetPosition} />
+                <ReactLogo position={sizes.reactLogoPosition} />
+                <Cube position={sizes.cubePosition} />
+                <RustLogo position={sizes.ringPosition} />
+            </group>
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
